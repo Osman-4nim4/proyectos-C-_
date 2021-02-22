c42cbf4b394e9941efec3f84202004363418710d
Osman <osmanpitufo112233@gmail.com>
2021-02-21 20:07:33 -0600

    Se creo hola mundo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace proyectos_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+            Console.ReadKey(); 
+        }
+    }
+}
